bb25ad3fbb484116a2c0e408a4172366f50de6a1
Evgeniya <rezinkina@list.ru>
2017-12-12 20:33:41 +0700

    reducers 1 part done, tests in process

--- src/sagas/loginSaga.js
+++ src/sagas/loginSaga.js
@@ -1,6 +1,7 @@
 import {call, put, takeLatest, fork, all} from 'redux-saga/effects';
 import {loginRequest, loginSuccess, loginFailure} from '../actions/loginActions';
-import {login} from '../helpers/api';
+import {registRequest, registSuccess, registFailure} from '../actions/registActions';
+import {login, registration} from '../helpers/api';
 
 function* fetchLogin(action) {
 
@@ -19,9 +20,27 @@ function* loginRequestWatch() {
     yield takeLatest(loginRequest, fetchLogin);
 }
 
+function* fetchRegist(action) {
+
+    try {
+        const {email, pass} = action.payload
+        const registResult = yield call(registration, action.payload);
+        console.log(registResult)
+        yield put(registSuccess(registResult));
+    } catch (error) {
+        console.log(error)
+        yield put(registFailure(error));
+    }
+}
+
+function* registRequestWatch() {
+    yield takeLatest(registRequest, fetchRegist);
+}
+
 
 export default function* loginSaga () {
     yield all([
         fork(loginRequestWatch),
+        fork(registRequestWatch),
     ])
 }

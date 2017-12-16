b3d9e5a423df523b816719fd9f89ff333e3792a5
Evgeniya <rezinkina@list.ru>
2017-12-16 21:13:28 +0700

    change currency of link value

--- src/components/InnerPage/InnerPage.js
+++ src/components/InnerPage/InnerPage.js
@@ -1,4 +1,8 @@
 import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { withRouter, Link } from "react-router-dom";
+import {selectBtc, selectEth} from '../../actions/currencyActions';
 
 import Header from './Header'
 import Content from './Content'
@@ -7,6 +11,29 @@ import Footer from './Footer'
 import './innerPage.css'
 
 class InnerPage extends PureComponent{
+
+    setCurrency = (currency) => {
+        const { selectBtc, selectEth } = this.props;
+        if (currency === "btc") {
+            selectBtc();
+        } else {
+            selectEth();
+        }
+    };
+
+    componentDidMount() {
+        const {currency} = this.props.match.params;
+        this.setCurrency(currency);
+    }
+
+    componentWillReceiveProps(newProps) {
+        const {currency} = this.props.match.params;
+        const newCurrency = newProps.match.params.currency;
+        if (currency && currency !== newCurrency) {
+            this.setCurrency(newCurrency);
+        }
+    }
+
 
     render(){
 
@@ -21,4 +48,25 @@ class InnerPage extends PureComponent{
 
 }
 
-export default InnerPage
+InnerPage.propTypes = {
+    selectBtc: PropTypes.func,
+    selectEth: PropTypes.func,
+};
+
+const mapStateToProps = state => ({
+});
+
+const mapDispatchToProps = dispatch => {
+    return {
+        selectBtc: () => {
+        dispatch(selectBtc());
+    },
+    selectEth: () => {
+        dispatch(selectEth());
+    },
+};
+};
+
+export default withRouter(
+    connect(mapStateToProps, mapDispatchToProps)(InnerPage)
+);
